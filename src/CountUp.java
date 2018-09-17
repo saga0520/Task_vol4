f91dd48443847ec264e7c69ebb88effd75031ce1
saga <saga0520@example.com>
2018-09-17 14:23:26 +0900

    カウンターを画面サイズに合わせて動的に移動する修正

--- src/CountUp.java
+++ src/CountUp.java
@@ -1,68 +1,52 @@
-import java.awt.EventQueue;
-import java.awt.Label;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.awt.BorderLayout;
+import java.awt.Container;
+import java.awt.Frame;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
-public class CountUp {
+public class CountUp extends Frame{
 	static int count = 0;
+	static JLabel label;
+
 	public static void main(String[] args){
 
-		//イベントディスパッチスレッドでSwingの描画処理を行う
-		EventQueue.invokeLater(new Runnable(){
-			public void run(){
-				ActionButton actBtn = new ActionButton();
-				actBtn.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-				actBtn.setVisible(true);
-			}
-		});
-	}
-}
+		//ウィンドウの設定
+		JFrame frame = new JFrame();
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setSize(400,300);
+		frame.setLocationRelativeTo(null);
 
-//serialの警告を抑止
-@SuppressWarnings("serial")
-class ActionButton extends JFrame implements ActionListener {
-	Integer count = 0;
-	public ActionButton(){
-
-		//ウインドウを設定
-		setTitle("課題");
-		setSize(400, 300);
-		setLocationRelativeTo(null);
-
-		//初期設定：カウント0回を表示するラベルの作成
-		Label label = new Label("0 回");
-		label.setBounds(180,100,100,100);
-		add(label);
-
-		//カウントアップボタンを押下した際の動作を設定
+		//カウントボタンを押下した際の動作を設定
 		JButton counter_button = new JButton("カウントアップ");
 		counter_button.addActionListener(e -> {
 			count++;
-			String str = count.toString();
-			label.setText(str + " 回");
+			label.setText(count + " 回");
 		});
 
 		//リセットボタンを押下した際の動作を設定
 		JButton reset_button = new JButton("リセット");
 		reset_button.addActionListener(e -> {
-			count = 0;
-			String str = count.toString();
-			label.setText(str + " 回");
+			label.setText("0 回");
 		});
 
-		//カウントアップボタンとリセットボタンを配置
+		//ボタン押下カウンターを配置
+		label = new JLabel("0 回");
+		label.setHorizontalAlignment(JLabel.CENTER);
+
+		//カウントアップボタン、リセットボタンを配置
 		JPanel buttonPanel;
 		buttonPanel = new JPanel();
 		buttonPanel.add(counter_button);
 		buttonPanel.add(reset_button);
-		add(buttonPanel);
-	}
-	@Override
-	public void actionPerformed(ActionEvent e) {
 
+		Container Container = frame.getContentPane();
+		Container.add(buttonPanel, BorderLayout.NORTH);
+		Container.add(label);
+
+		//可視化
+		frame.setVisible(true);
 	}
 }
